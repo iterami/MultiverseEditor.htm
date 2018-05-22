fee0f38cc820dc8a09c125ce92863ab7988a6c05
Jan Hořava <czechinthemail@gmail.com>
2018-05-22 13:19:30 -0700

    added prebuilt level loading

---
+++ js/data.js
@@ -0,0 +1,16 @@
+'use strict';
+
+function ajax_level(level){
+    core_ajax({
+      'todo': load_prebuilt_level,
+      'url': '../common/multiverse/' + level + '.json',
+    });
+}
+
+function load_prebuilt_level(responseText){
+    webgl_level_load({
+      'level': responseText,
+    });
+
+    core_escape();
+}
--- js/main.js
+++ js/main.js
@@ -6,15 +6,21 @@ function logic(){
 function repo_init(){
     core_repo_init({
       'events': {
-        'start': {
+        'load_file': {
           'onclick': function(){
               webgl_load_level({
                 'json': document.getElementById('json').files[0] || false,
               });
           },
         },
+        'load_prebuilt': {
+          'onclick': function(){
+              ajax_level(level_select.value);
+          },
+        },
       },
-      'info': '<input id=json type=file><input id=start type=button value="Load Level">',
+      'info': '<table><tr><td><input id=json type=file><td><input id=load_file type=button value="Load Level From File">'
+        + '<tr><td><select id=level_select><option value=trains>Trains</option></select><td><input id=load_prebuilt type=button value="Load Prebuilt Level"></table>',
       'keybinds': {
         32: {},
         67: {},
@@ -31,8 +37,13 @@ function repo_init(){
       'title': 'MultiverseEditor.htm',
     });
 
-    var level = window.location.search.substring(1);
-    if(level.length > 0){
-        core_escape();
+    if(window.location.protocol !== 'file:'){
+        var level = window.location.search.substring(1);
+        if(level.length > 0){
+            ajax_level(level);
+        }
+
+    }else{
+        document.getElementById('load_prebuilt').disabled = true;
     }
 }
