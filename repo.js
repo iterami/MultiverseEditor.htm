3434b27bc045b7b2bc1fe67391092c04d4a57c2f
Jan Hořava <czechinthemail@gmail.com>
2025-02-06 05:32:31 -0800

    removed unused event args

--- repo.js
+++ repo.js
@@ -589,7 +589,7 @@ function repo_init(){
           'preventDefault': true,
         },
         'mousemove': {
-          'todo': function(event){
+          'todo': function(){
               webgl_controls_mouse(webgl_character_id);
           },
         },
