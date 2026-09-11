bf75a669e2448556474726b1cc9205ccd32c7895
Jan Hořava <czechinthemail@gmail.com>
2026-09-11 08:17:32 -0700

    removed beforeunload usage

--- repo.js
+++ repo.js
@@ -357,12 +357,6 @@ function repo_escape(){
 
 function repo_init(){
     core_repo_init({
-      'beforeunload': function(event){
-          if(webgl !== 0){
-              core_escape(true);
-              event.preventDefault();
-          }
-      },
       'events': {
         'camera_zoom_set': {
           'onclick': function(){
